4af5ad7c565f9e16376d880f1b6937305fa20c3d
DonMB77 <purposefuldrifter@gmail.com>
2025-02-11 17:49:31 +0100

    html textbox

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ let fullName = "Full Name";
 let age = 25;
 let student = false;
 
-document.getElementById("p1").textContent = `My name is ${fullName}`;
-document.getElementById("p2").textContent = `My age is ${age}`;
-document.getElementById("p3").textContent = `I am a student: ${student}`;
+//document.getElementById("p1").textContent = `My name is ${fullName}`;
+//document.getElementById("p2").textContent = `My age is ${age}`;
+//document.getElementById("p3").textContent = `I am a student: ${student}`;
 
 // window prompt
-let username;
+/*let username;
 
 username = window.prompt(`What's your username?`);
 console.log(username);
+*/
+
+// html textbox
+let username;
+document.getElementById("mySubmit").onclick = function() {
+    username = document.getElementById("myText").value;
+    document.getElementById("nameH1").textContent = username;
+}
