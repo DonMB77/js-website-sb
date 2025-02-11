268aebc7d1a4347e952fd70260fd4e25565b67fa
DonMB77 <purposefuldrifter@gmail.com>
2025-02-11 17:29:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,11 @@
 
 document.getElementById("myH1").textContent = "Hello World!";
 document.getElementById("myP").textContent = "I like pizza!";
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(`You are ${age} years old`);
+console.log(`The price of this item is ${price}`);
+console.log(`Your GPA is ${gpa}`);
