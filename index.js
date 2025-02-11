7425b09ca9c8cd662cd70693b6b2e708c426d958
DonMB77 <purposefuldrifter@gmail.com>
2025-02-11 17:36:53 +0100

    sb

--- index.js
+++ index.js
@@ -4,13 +4,23 @@
 //window.alert(`This is an alert!`);
 //window.alert(`I like pizza!`);
 
-document.getElementById("myH1").textContent = "Hello World!";
+/*document.getElementById("myH1").textContent = "Hello World!";
 document.getElementById("myP").textContent = "I like pizza!";
 
 let age = 25;
 let price = 10.99;
 let gpa = 2.1;
 
+console.log(typeof(price));
 console.log(`You are ${age} years old`);
 console.log(`The price of this item is ${price}`);
 console.log(`Your GPA is ${gpa}`);
+*/
+
+let fullName = "Full Name";
+let age = 25;
+let student = false;
+
+document.getElementById("p1").textContent = `My name is ${fullName}`;
+document.getElementById("p2").textContent = `My age is ${age}`;
+document.getElementById("p3").textContent = `I am a student: ${student}`;
